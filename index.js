45f570879aac761483a8297903fd0becc0c80c5c
Prasanth138 <prasanthanbu777@gmail.com>
2022-09-23 22:34:40 +0530

    update

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ import UserRoutes from "./routes/usersRoutes.js";
 import BookingRoutes from "./routes/bookingRoutes.js";
 
 // Middleware 
-app.use(cors({origin: '*'}));
+app.use(cors());
+// app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(cookieParser());
 
